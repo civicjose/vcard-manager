5f84ac73472ae2a0b9201c2a1802ce3a12e915f7
Jose <jcivicog@gmail.com>
2024-09-25 13:47:13 +0200

    Funcional

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
 
-=======
-import './bootstrap';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
->>>>>>> 2736f2f813a1498ed8ccc38039a773e44d63b147
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
   content: [
     './resources/**/*.blade.php',
@@ -12,26 +11,3 @@ module.exports = {
 };
 
 
-=======
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [forms],
-};
->>>>>>> 2736f2f813a1498ed8ccc38039a773e44d63b147
